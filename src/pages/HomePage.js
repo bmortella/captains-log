ecd4bf4b5c6665d014b48c4269e61dd1b1aecc7b
Bruno Mortella <bmortella@gmail.com>
2022-04-04 19:33:44 -0300

    Add new log page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,36 +1,5 @@
-import { useState } from "react";
-import { Form, FormGroup, Label, Input, Button } from "reactstrap";
-import { insertLog } from "../api";
-
 function HomePage() {
-  const [newLog, setNewLog] = useState("");
-
-  function handleChange(event) {
-    setNewLog(event.target.value);
-  }
-
-  function insert() {
-    insertLog(newLog).then((response) => {
-      console.log(response);
-    });
-  }
-
-  return (
-    <div>
-      <Form>
-        <FormGroup>
-          <Label for="exampleText">Log</Label>
-          <Input
-            id="exampleText"
-            name="text"
-            type="textarea"
-            onChange={handleChange}
-          />
-        </FormGroup>
-        <Button onClick={insert}>Submit</Button>
-      </Form>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default HomePage;
